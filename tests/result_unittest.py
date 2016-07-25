7ffa8d0578eb1c26015c721212a65a39b4bf85eb
doyin <Adedoyin.Oketola@rackspace.com>
2016-07-25 16:11:13 -0500

    unit tests for canary results

--- tests/result_unittest.py
+++ tests/result_unittest.py
@@ -76,8 +76,8 @@ class ResultTest(unittest.TestCase):
 
     @staticmethod
     def generate_fake_result():
-        data = {'status' : 'pass',
-                'failure_details' : ""
+        data = {'status': 'pass',
+                'failure_details': ""
                 }
         return data
 
@@ -93,7 +93,6 @@ class ResultTest(unittest.TestCase):
         json_data = json.loads(result.data)
         self.assertEquals(json_data.get('status'), 'pass', "result status does not match")
         self.assertEquals(json_data.get('id'), 1, "result id does not match")
-
 
     def test_get_result(self):
         project = self.post_fake_project()
@@ -111,40 +110,54 @@ class ResultTest(unittest.TestCase):
         self.assertEquals(json_data.get('id'), 1, "Test_Get_Result: Result ID does not match")
         self.assertEquals(json_data.get('status'), 'pass', "Test_Get_Result : Result status does not match")
 
-
-    def test_get_canaries(self):
+    def test_get_results(self):
         project = self.post_fake_project()
         self.assertEquals(project.status_code, 201)
         self.assertEquals(json.loads(project.data).get('id'), 1)
         canary = self.post_fake_canary()
         self.assertEquals(canary.status_code, 201)
         self.assertEquals(json.loads(canary.data).get('id'), 1)
-        
-        get_canary = self.client.get('api/projects/1/canary', content_type='application/json')
+        results = 3
+        for i in range(0, results):
+            self.post_fake_result()
+        get_canary = self.client.get('api/projects/1/canary/1/results', content_type='application/json')
         self.assertEquals(get_canary.status_code, 200)
+        self.assertEquals(len(json.loads(get_canary.data).get('results')), results,
+                          "Test_Get_Results: Number of results do not match")
         # DO MORE CHECKS
 
-    # def test_edit_canary(self):
-    #     fake_project = self.post_fake_project()
-    #     self.assertEquals(fake_project.status_code, 201)
-    #     self.post_fake_canary()
-    #     edit_response = self.client.put('api/projects/1/canary/1', data=json.dumps(
-    #         {'criteria': {'threshold': '100%', 'expected_run_frequency': '5hrs', 'result_sample_size': '10'}}),
-    #                                     headers={'content-type': 'application/json'})
-    #
-    #     self.assertEquals(edit_response.status_code, 200)
-    #     response_data = json.loads(edit_response.data)
-    #     self.assertEquals(response_data.get('criteria'),
-    #                       {'threshold': '100%', 'expected_run_frequency': '5hrs', 'result_sample_size': '10'},
-    #                       "EXPECTED {}, GOT {}".format({'threshold': '100%'}, response_data.get('criteria')))
-    #
-    # def test_delete_canary(self):
-    #     fake_project = self.post_fake_project()
-    #     self.assertEquals(fake_project.status_code, 201)
-    #     self.post_fake_canary()
-    #     delete_canary = self.client.delete('api/projects/1/canary/1')
-    #     self.assertEquals(delete_canary.status_code, 200)  # disabled the canary
-    #     get_canary = self.client.get('api/projects/1/canary/1', content_type='application/json')
-    #     self.assertEquals(get_canary.status_code, 200)
-    #     json_data = json.loads(get_canary.data)
-    #     self.assertEquals(json_data.get('status'), "DISABLED", "ERROR : Canary not disabled")
+    def test_edit_result(self):
+        project = self.post_fake_project()
+        self.assertEquals(project.status_code, 201)
+        self.assertEquals(json.loads(project.data).get('id'), 1)
+        canary = self.post_fake_canary()
+        self.assertEquals(canary.status_code, 201)
+        self.assertEquals(json.loads(canary.data).get('id'), 1)
+        result = self.post_fake_result()
+        self.assertEquals(result.status_code, 201)
+        self.assertEquals(json.loads(result.data).get('id'), 1)
+        edit_result = self.client.put('api/projects/1/canary/1/results/1', data=json.dumps(
+            {'status': 'fail'}), headers={'content-type': 'application/json'})
+
+        self.assertEquals(edit_result.status_code, 200)
+        json_data = json.loads(edit_result.data)
+        self.assertEquals(json_data.get('status'), 'fail',
+                          "EXPECTED {}, GOT {}".format('fail', json_data.get('status')))
+        self.assertEquals(json_data.get('failure_details'), "",
+                          "EXPECTED {}, GOT {}".format("", json_data.get('failure_details')))
+
+    def test_delete_result(self):
+        project = self.post_fake_project()
+        self.assertEquals(project.status_code, 201)
+        self.assertEquals(json.loads(project.data).get('id'), 1)
+        canary = self.post_fake_canary()
+        self.assertEquals(canary.status_code, 201)
+        self.assertEquals(json.loads(canary.data).get('id'), 1)
+        result = self.post_fake_result()
+        self.assertEquals(result.status_code, 201)
+        self.assertEquals(json.loads(result.data).get('id'), 1)
+        wrong_delete = self.client.delete('api/projects/1/canary/2/results/1')
+        self.assertEquals(wrong_delete.status_code, 404)
+        self.assertEquals(json.loads(wrong_delete.data).get('message'), "result not found")
+        delete_result = self.client.delete('api/projects/1/canary/1/results/1')
+        self.assertEquals(delete_result.status_code, 204)
