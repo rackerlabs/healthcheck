4e5d04412944dabcc92f7b89ba1f00c0f5034e2b
doyin <Adedoyin.Oketola@rackspace.com>
2016-08-09 18:24:33 -0500

    Pep8 fixes for api_client

--- app/worker/clients/api_client.py
+++ app/worker/clients/api_client.py
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from data_client import DataClient
-import requests, json
+import requests
+import json
 
 
 class APIClient(DataClient):
